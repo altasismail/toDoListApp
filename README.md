f24299528de67d354e4cc02fd2a4959d9dd2d38b
altasismail <altasismail@gmail.com>
2020-04-21 23:00:32 +0200

    simple note taking app php ended

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # toDoListApp
 php and Mysql - to do list web app
+
+I used a MSQL DataBase
+as 
+DB Name: "todo" in format "utf8mb4_general_ci
+table name: "tasks" with two rows
+row1 | id   | int   | primary  | A_I
+row2 | task | text  
+
+as simle as it is full project is coded in php - html - css
+
--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
-//connect to db
-$db = mysqli_connect("localhost", "root", "", "todo");
-if (isset($_POST["submit"])) {
-    $task = $_POST["task"];
-    mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
-    header('location:index.php');
-}
-$tasks = mysqli_query($db, "SELECT * FROM tasks");
+    //connect to db
+    $db = mysqli_connect("localhost", "root", "", "todo");
+    if (isset($_POST["submit"])) {
+        $task = $_POST["task"];
+        if (empty($task)) {
+            $errors = "Must be filled";
+        } else {
+            mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
+            header('location:index.php');
+        }
 
+    }
 
+    //delete task 
+    if(isset($_GET['del_task'])) {
+        $id = $_GET['del_task'];
+        mysqli_query($db, "DELETE FROM tasks WHERE id=$id");
+        header('location: index.php');
 
+    }
+    $tasks = mysqli_query($db, "SELECT * FROM tasks");
 
 ?>
 
@@ -29,6 +39,9 @@ $tasks = mysqli_query($db, "SELECT * FROM tasks");
 
     </div>
     <form method="POST" action="index.php">
+        <?php if (isset($errors)) {?>
+            <p><?php echo $errors; ?></p>
+        <?php } ?>
         <input type="text" name="task" class="task_input">
         <button type="submit" class="add_btn" name="submit">Add New Task</button>
     
@@ -44,15 +57,15 @@ $tasks = mysqli_query($db, "SELECT * FROM tasks");
             </tr>
         </thead>
         <tbody>
-            <?php while ($row = mysqli_fetch_array($tasks)) { ?>
+            <?php $i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
             <tr>
-                <td><?php echo $row ["id"]; ?></td>
+                <td><?php echo $i; ?></td>
                 <td class="task"><?php echo $row ["task"]; ?></td>
                 <td class="delete">
-                    <a href="#">x</a>
+                    <a href="index.php?del_task=<?php echo $row['id']; ?>">x</a>
                 </td>
             </tr>
-            <?php } ?>
+            <?php $i++; } ?>
         </tbody>
     
     </table>
